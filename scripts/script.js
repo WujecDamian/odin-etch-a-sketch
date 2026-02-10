22d482e1241fcee580bf8ca4cea004acb31e9dae
WujecDamian <wujecdamian12@gmail.com>
2026-02-11 00:56:43 +0100

    feat: Add RGB colors and opacity on hover +0.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 let container = document.querySelector('.container')
+let buttonNewGrid = document.querySelector('#grid-button')
 
 const gridBlock = document.createElement('div')
 gridBlock.classList.add('grid__block')
 
+function randomRGB () {
+  return Math.floor(Math.random() * 255)
+}
 function newGrid (blocksCount) {
   let calculateWidth = 960 / blocksCount - 1
+
   for (let i = 0; i < blocksCount * blocksCount; i++) {
     const gridBlock = document.createElement('div')
     gridBlock.classList.add('grid__block')
@@ -12,11 +17,30 @@ function newGrid (blocksCount) {
       'style',
       `width: ${calculateWidth}px; max-width:${calculateWidth}`
     )
+    let opacity = 0
+
     gridBlock.addEventListener('mouseenter', () => {
-      gridBlock.style.backgroundColor = 'white'
+      opacity += 0.1
+
+      gridBlock.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
+      gridBlock.style.opacity = `${opacity}`
     })
+
     container.append(gridBlock)
     console.log(gridBlock.style)
   }
 }
-newGrid(54) // x^2
+newGrid(32) // x^2
+buttonNewGrid.addEventListener('click', () => {
+  let allGridBlocks = document.querySelectorAll('.grid__block')
+  allGridBlocks.forEach(block => {
+    block.remove()
+  })
+  let customBlocksCounts = prompt(
+    'Give number od block in row. ( eg.16 = 16x16 grid )'
+  )
+  while (customBlocksCounts > 100) {
+    customBlocksCounts = prompt('Give number not bigger than 100')
+  }
+  newGrid(customBlocksCounts)
+})
