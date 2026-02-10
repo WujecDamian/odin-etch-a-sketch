a5276df1d21876b4262d5d41c2c86f3119cf2c72
WujecDamian <wujecdamian12@gmail.com>
2026-02-11 00:31:02 +0100

    feat: Add newGrid function

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,20 @@ let container = document.querySelector('.container')
 const gridBlock = document.createElement('div')
 gridBlock.classList.add('grid__block')
 
-function newGrid (blocksCount) {}
+function newGrid (blocksCount) {
+  let calculateWidth = 960 / blocksCount - 1
+  for (let i = 0; i < blocksCount * blocksCount; i++) {
+    const gridBlock = document.createElement('div')
+    gridBlock.classList.add('grid__block')
+    gridBlock.setAttribute(
+      'style',
+      `width: ${calculateWidth}px; max-width:${calculateWidth}`
+    )
+    gridBlock.addEventListener('mouseenter', () => {
+      gridBlock.style.backgroundColor = 'white'
+    })
+    container.append(gridBlock)
+    console.log(gridBlock.style)
+  }
+}
+newGrid(54) // x^2
